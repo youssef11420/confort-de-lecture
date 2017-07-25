e1f0b1a08227cf89f6cb58a3de013c0fdc29c08f
Youssef Imzourh <yimzourh@ayaline.com>
2017-07-25 15:32:15 +0200

    Evolution 3.0.1 : - corrections CSS images

--- templates/js/title.js
+++ templates/js/title.js
@@ -97,6 +97,12 @@ jQuery(document).ready(function () {
         zindex: 114200
     });
 
+    jQuery(".cdlCadre img").each(function () {
+        if (jQuery(this).width() < 600) {
+            jQuery(this).width(Math.min(jQuery(this).width() * 2, 600));
+        }
+    });
+
     jQuery(document).on("click", ".cdlGalleryClose a", function (event) {
         jQuery(".cdlImageGallery").hide();
         event.preventDefault();
@@ -161,15 +167,5 @@ jQuery(document).ready(function () {
         jQuery("html, body").animate({scrollTop: 0});
         e.preventDefault();
         return false;
-    });
-});
-
-jQuery(window).load(function () {
-    "use strict";
-
-    jQuery(".cdlCadre img").each(function () {
-        if (jQuery(this).width() < 600) {
-            jQuery(this).width(Math.max(jQuery(this).width() * 2, 600));
-        }
     });
 });
